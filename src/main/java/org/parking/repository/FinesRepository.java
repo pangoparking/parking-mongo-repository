e4b847bcc367ef726108d8378d4a4700bffd2715
Michael <m.shac86@gmail.com>
2023-05-06 22:28:18 +0300

    added query byOwnerID

--- src/main/java/org/parking/repository/FinesRepository.java
+++ src/main/java/org/parking/repository/FinesRepository.java
@@ -1,8 +1,20 @@
 package org.parking.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.parking.FineDoc;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface FinesRepository extends MongoRepository<FineDoc, Long> {
+
+	
+	List<FineDoc> findAllByOwnerID(long ownerID);
+
+	boolean existsByOwnerID(long ownerID);
+	
+	@Query(value = "{$and:[{ownerID:?0}, {dateTime:{$gte:?1, $lte:?2}}]}")
+	List<FineDoc> findAllFinesByOwnerIDAndDateTimeInterval(long ownerID, LocalDateTime from, LocalDateTime to);
 
 }
