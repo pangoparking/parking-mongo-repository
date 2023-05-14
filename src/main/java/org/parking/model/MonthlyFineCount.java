aabfb18048745270bee88aeb6e10e24f69a2524f
Michael <m.shac86@gmail.com>
2023-05-14 20:26:19 +0300

    added methods to FineRepository

---
+++ src/main/java/org/parking/model/MonthlyFineCount.java
@@ -0,0 +1,12 @@
+package org.parking.model;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MonthlyFineCount {
+
+	private Integer month;
+    private Integer count;
+	
+}
